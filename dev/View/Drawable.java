edc5ef90919511c22602abda2aeb26bf2281eab8
Lucas Fonseca dos Santos <fonsecalucassantos@gmail.com>
2021-03-04 17:14:45 +0000

    Documenting AnimatedView and Drawable class by javadoc standart.

--- dev/View/Drawable.java
+++ dev/View/Drawable.java
@@ -1,7 +1,17 @@
 package View;
 
+/**
+ * Implementa uma interface de representação na interface
+ * gráfica para o usuário.
+ * 
+ * @author Grupo
+ * @version 1.0 SNAPSHOT
+ */
 public interface Drawable {
 
+    /**
+     * Método de renderização.
+     */
     public abstract void draw();
 
 }
