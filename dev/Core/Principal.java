da46d147f0186d3895c8a3a243b709dbfb492e01
Lucas Fonseca dos Santos <fonsecalucassantos@gmail.com>
2021-03-04 15:56:06 +0000

    Documenting Principal class by javadoc standart.

--- dev/Core/Principal.java
+++ dev/Core/Principal.java
@@ -1,9 +1,26 @@
 package Core;
 
+/**
+ * Classe responsável pela inicialização das rotinas de simulação.
+ * Sua única serventia é ser chamado pela JVM e instanciar um Simulator
+ * responsável por todo o encapsulamento do projeto.
+ * 
+ * @author Grupo
+ * @version 1.0 SNAPSHOT
+ */
 public class Principal {
+
+  /**
+   * Método chamado pela JVM para execução do código.
+   * 
+   * @params args Argumentos via cli.
+   */
   public static void main(String[] args) {
+
     Simulator simulator = new Simulator();
     simulator.runLongSimulation();
     //simulator.simulate();
+
   }
+
 }
